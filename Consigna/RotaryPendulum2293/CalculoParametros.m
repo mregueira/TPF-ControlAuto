040cd497d61ee0f017a6fb0bf99434a8af7e3e47
mregueira <mregueira@itba.edu.ar>
2021-01-19 20:04:57 -0300

    agregamos controles no anda aun

--- Consigna/RotaryPendulum2293/CalculoParametros.m
+++ Consigna/RotaryPendulum2293/CalculoParametros.m
@@ -1,4 +1,5 @@
 %% Experimentos para cálculo de parámetros
+clc;
 g = 9.81;
 %% B) J1 - Momento de inercia 1
 tau_1 = 1;
@@ -25,7 +26,32 @@ J2 = tau_2/tdd_2;
 
 %% Linealizacion de ecuaciones de estado
 %b1 y b2:damping coefficient 
-b1 = 0;     %cambiar
-b2 = 0;     %cambiar
-L1 = 2*l1;  %cambiar
-theta = linealizacion(m1,m2,l1,l2,L1,J1,J2,tau_1,b1,b2,g);
+b1 = 1e-6; %cambiar
+b2 = 0; %cambiar
+L1 = 103.5e-3;  % Abrimos las dos piezas con un visualizador
+[theta,A,B] = linealizacion(m1,m2,l1,l2,L1,J1,J2,tau_1,b1,b2,g);
+C = [1 1 0 0];
+%% Realimentacion de estados (No estaria andando, se queda oscilando)
+% Pruebo sin accion integral
+% K = acker(A,B,[-10;-10;-10;-10]);
+% Con accion integral
+% Ba = [B;0];
+% Aa = [A zeros(4,1);-C 0];
+% Kt = acker(Aa,Ba,[-10;-10;-10;-10;-10]);
+% K = Kt(1:end-1)
+% Ki = -Kt(end)
+% Prueba con LQR (tampoco anda)
+% Q = diag([10 10 10 10]);
+% R = 5;
+% [K,S,e] = lqr(A,B,Q,R);
+%% LoopShaping
+% Primero sacamos la transferencia SIMO
+C = [1 0 0 0];
+[n,d] = ss2tf(A,B,C,0);
+P1 = tf(n,d);
+C = [0 1 0 0];
+[n,d] = ss2tf(A,B,C,0);
+P2 = tf(n,d);
+% Disenio controlador C2
+s = tf('s');
+C2 = (1/s);
--- Consigna/RotaryPendulum2293/linealizacion.m
+++ Consigna/RotaryPendulum2293/linealizacion.m
@@ -1,4 +1,4 @@
-function theta = linealizacion(m1,m2,l1,l2,L1,J1,J2,tau_1,b1,b2,g)
+function [theta,A,B] = linealizacion(m1,m2,l1,l2,L1,J1,J2,tau_1,b1,b2,g)
 %% Ecuaciones de estado linealizadas 
 % Se utiliza la ecuacion (36) del paper "On the Dynamics of the Furuta Pendulum"
 A31 = 0;
@@ -13,10 +13,12 @@ B31 = J2 / (J1*J2 - (m2*L1*l2)^2);
 B41 = (m2*L1*l2) / (J1*J2 - (m2*L1*l2)^2);
 
 syms t_1 t_2 td_1 td_2;
-tdd_1 = [A31 A32 A33 A34]*[t_1; t_2; td_1; td_2] + B31*tau_1
+tdd_1 = [A31 A32 A33 A34]*[t_1; t_2; td_1; td_2] + B31*tau_1;
 tdd_2 = [A41 A42 A43 A44]*[t_1; t_2; td_1; td_2] + B41*tau_1;
 
 theta = [td_1; td_2; tdd_1; tdd_2];
+A = [0 0 1 0;0 0 0 1;A31 A32 A33 A34;A41 A42 A43 A44];
+B = [0;0;B31;B41];
 end
 
 
