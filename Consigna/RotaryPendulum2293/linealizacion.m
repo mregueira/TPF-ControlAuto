ca41bf944d3ec9d92d791586f3c26f03fbbae6c6
mregueira <mregueira@itba.edu.ar>
2021-01-21 16:41:02 -0300

    ahi anda en continuo
    falta que ande digitalizado y loopShaping

--- Consigna/RotaryPendulum2293/linealizacion.m
+++ Consigna/RotaryPendulum2293/linealizacion.m
@@ -1,16 +1,19 @@
 function [theta,A,B] = linealizacion(m1,m2,l1,l2,L1,J1,J2,tau_1,b1,b2,g)
 %% Ecuaciones de estado linealizadas 
 % Se utiliza la ecuacion (36) del paper "On the Dynamics of the Furuta Pendulum"
+j0s = J1 + m1*(l1^2) + m2*(L1^2);
+j2s = J2 + m2*(l2^2);
+
 A31 = 0;
-A32 = (g*L1*(m2*l2)^2) / (J1*J2);
-A33 = (-b1*J2) / (J1*J2 - (m2*L1*l2)^2); 
-A34 = (-b2*m2*l2*L1) / (J1*J2 - (m2*L1*l2)^2);
+A32 = (g*L1*(m2*l2)^2) / (j0s*j2s - (m2*L1*l2)^2);
+A33 = (-b1*j2s) / (j0s*j2s - (m2*L1*l2)^2); 
+A34 = (-b2*m2*l2*L1) / (j0s*j2s - (m2*L1*l2)^2);
 A41 = 0;
-A42 = (g*m2*l2*J1) / (J1*J2 - (m2*L1*l2)^2);
-A43 = (-b1*m2*l2*L1) / (J1*J2 - (m2*L1*l2)^2);
-A44 = (-b2*J1) / (J1*J2 - (m2*L1*l2)^2);
-B31 = J2 / (J1*J2 - (m2*L1*l2)^2);
-B41 = (m2*L1*l2) / (J1*J2 - (m2*L1*l2)^2);
+A42 = (g*m2*l2*j0s) / (j0s*j2s - (m2*L1*l2)^2);
+A43 = (-b1*m2*l2*L1) / (j0s*j2s - (m2*L1*l2)^2);
+A44 = (-b2*j0s) / (j0s*j2s - (m2*L1*l2)^2);
+B31 = j2s / (j0s*j2s - (m2*L1*l2)^2);
+B41 = (m2*L1*l2) / (j0s*j2s - (m2*L1*l2)^2);
 
 syms t_1 t_2 td_1 td_2;
 tdd_1 = [A31 A32 A33 A34]*[t_1; t_2; td_1; td_2] + B31*tau_1;
